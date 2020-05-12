88803a06ea7453235d7a8c1f39f4ff07e6e069c9
Guto Aielo <guhamm@hotmail.com>
2020-05-12 18:16:04 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/guhamm/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.guhamm.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guhamm.cursomc.domain.Categoria;
+import com.guhamm.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+		
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+	
+	}

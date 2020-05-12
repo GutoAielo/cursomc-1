e32e4b734f948a846042a9a64d3433121d9ccd32
Guto Aielo <guhamm@hotmail.com>
2020-05-12 15:33:25 -0300

    Projeto criado

---
+++ src/test/java/com/guhamm/cursomc/Cursomc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guhamm.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursomc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

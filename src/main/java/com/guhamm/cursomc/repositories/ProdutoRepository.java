d739bf5daecc67891af131a61af491559491fc73
Guto Aielo <guhamm@hotmail.com>
2020-05-12 19:35:32 -0300

    Produto associacao ManyToMany

---
+++ src/main/java/com/guhamm/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.guhamm.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.guhamm.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}

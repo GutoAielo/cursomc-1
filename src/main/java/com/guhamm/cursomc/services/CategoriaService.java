1663979764326948574bdbf5016a7a94aa1ea68a
Guto Aielo <guhamm@hotmail.com>
2020-05-13 12:30:27 -0300

    Ajustes endpoint/Categoria/{id}

--- src/main/java/com/guhamm/cursomc/services/CategoriaService.java
+++ src/main/java/com/guhamm/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.guhamm.cursomc.domain.Categoria;
 import com.guhamm.cursomc.repositories.CategoriaRepository;
+import com.guhamm.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,7 @@ public class CategoriaService {
 		
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 	

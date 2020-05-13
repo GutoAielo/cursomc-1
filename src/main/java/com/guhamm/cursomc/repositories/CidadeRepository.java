2a532752a5904ada12b9bdd152ba804b29448e8e
Guto Aielo <guhamm@hotmail.com>
2020-05-13 13:13:10 -0300

    Estado e Cidade

---
+++ src/main/java/com/guhamm/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.guhamm.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.guhamm.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
